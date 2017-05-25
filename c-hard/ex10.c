0b51d48d94e3ca0f2c02e083da992009e95ad9fc
mairandomness <mairakodama@gmail.com>
2017-05-25 13:31:36 -0300

    added ex 10

---
+++ c-hard/ex10.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+
+int lower(int a);
+/* int lower(int a) { */
+/* 	if ((a>=65)&&(a<=90)){ */
+/* 		a+=32; */
+
+/* 		return a; */
+/* 	} */
+/* 	else { */
+/* 		return 0; */
+/* 	} */
+/* } */
+
+int main(int argc, char *argv[])
+{
+	if (argc !=2) {
+		printf("ERROR: Ypi need another argument. \n");
+		//this is how you abort a program
+		return 1;
+	}
+	int i=0;
+	char letter;
+	for (i=0; argv[1][i]!='\0'; i++	, letter=argv[1][i])		 {
+
+	/* char letter=argv[1][i]; */
+		lower(letter);
+		switch (letter) {
+			case 'a' :
+			/* case 'A' : */
+				printf("%d: 'A'\n", i);
+				break;
+
+			case 'e' :
+			/* case 'E' : */
+				printf("%d: 'E'\n", i);
+				break;
+
+			case 'i' :
+			/* case 'I' : */
+				printf("%d: 'I'\n", i);
+				break;
+
+			case 'o' :
+			/* case 'O' : */
+						printf("%d: 'O'\n", i);
+						break;
+
+			case 'u' :
+			/* case 'U' : */
+						printf("%d: 'U'\n", i);
+						break;
+
+			case 'y' :
+			/* case 'Y' : */
+						if (i>2) {
+							// it's only sometimes Y
+							printf("%d: 'Y'\n", i);
+						}
+						break;
+
+			default:
+						printf("%d: %c is not a vowel\n", i, letter);
+		}
+	}
+		return 0;
+}
+
+
+int lower(int a) {
+	if ((a>=65)&&(a<=90)){
+		a+=32;
+
+		return a;
+	}
+	else {
+		return 0;
+	}
+}
