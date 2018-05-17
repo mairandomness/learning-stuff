1a1df9207c3b6f1eea37a1e3bd58731567fb91a1
mairandomness <mairakodama@gmail.com>
2018-05-17 16:47:38 -0300

    solved a couple of udacity's exercises

--- Udacity-fullstack/course-ud303/Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
+++ Udacity-fullstack/course-ud303/Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
@@ -24,12 +24,15 @@ from urllib.parse import parse_qs
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # 1. How long was the message? (Use the Content-Length header.)
-
+        length = int(self.headers.get('Content-length', 0))
         # 2. Read the correct amount of data from the request.
-
+        data = self.rfile.read(length).decode()
         # 3. Extract the "message" field from the request data.
+        message = parse_qs(data)["message"][0]
 
         # Send the "message" field back as the response.
+
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
