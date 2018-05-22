181ef49e37d28dfa3900a112d390b3728f8c42ee
mairandomness <mairakodama@gmail.com>
2018-05-22 17:10:09 -0300

    add soultion to json udacity exercise

--- Udacity-fullstack/course-ud303/Lesson-2/6_UsingJSON/UINames.py
+++ Udacity-fullstack/course-ud303/Lesson-2/6_UsingJSON/UINames.py
@@ -15,9 +15,12 @@ def SampleRecord():
     r = requests.get("http://uinames.com/api?ext&region=United%20States",
                      timeout=2.0)
     # 1. Add a line of code here to decode JSON from the response.
-
+    fields = r.json()
     return "My name is {} {} and the PIN on my card is {}.".format(
         # 2. Add the correct fields from the JSON data structure.
+        fields["name"],
+        fields["surname"],
+        fields["credit_card"]["pin"]
     )
 
 if __name__ == '__main__':
